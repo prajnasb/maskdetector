1816531240422b06b43426db28b90f7672d1b9ee
prajnasb <61310612+prajnasb@users.noreply.github.com>
2020-04-19 01:06:30 +0530

    Update voc.py

--- mmdetection/mmdet/datasets/voc.py
+++ mmdetection/mmdet/datasets/voc.py
@@ -6,10 +6,7 @@ from .xml_style import XMLDataset
 @DATASETS.register_module
 class VOCDataset(XMLDataset):
 
-    CLASSES = ('aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car',
-               'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse',
-               'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train',
-               'tvmonitor')
+    CLASSES = ('mask','no_mask)
 
     def __init__(self, **kwargs):
         super(VOCDataset, self).__init__(**kwargs)
