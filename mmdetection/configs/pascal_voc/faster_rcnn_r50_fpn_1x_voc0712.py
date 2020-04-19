7d43f920b392b37a04b8b5db0b3f1787eb1b04cf
prajnasb <61310612+prajnasb@users.noreply.github.com>
2020-04-19 23:08:48 +0530

    Update faster_rcnn_r50_fpn_1x_voc0712.py

--- mmdetection/configs/pascal_voc/faster_rcnn_r50_fpn_1x_voc0712.py
+++ mmdetection/configs/pascal_voc/faster_rcnn_r50_fpn_1x_voc0712.py
@@ -100,7 +100,7 @@ test_cfg = dict(
 )
 # dataset settings
 dataset_type = 'VOCDataset'
-data_root = 'data/VOC2007'
+data_root = 'data/VOCdevkit/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
